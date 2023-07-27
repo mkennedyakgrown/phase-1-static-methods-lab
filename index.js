d48831778266460bd15855ea69e35c102fa4d890
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-07-26 21:05:10 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    let newString = string.charAt(0).toUpperCase() + string.slice(1);
+    return newString.replace(
+      /\w\S*/g,
+      function(txt) {
+        const noCaps = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+        if (noCaps.indexOf(txt) < 0) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        } else {
+          return txt;
+        }
+      }
+    );
+  }
 }
